26bf4bffbbab29e31ec92d07943e461b20e5d38e
shwld <shu.account@outlook.com>
2022-08-25 05:56:02 +0900

    test is fail

--- contracts/AnniversaryToken.sol
+++ contracts/AnniversaryToken.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "hardhat/console.sol";
+
 import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 
