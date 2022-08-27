4f454e08bc5dc32952780fc58879384532a95825
shwld <shu.account@outlook.com>
2022-08-28 06:30:06 +0900

    ✨feat: add deploy command

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const AnniversaryToken = await ethers.getContractFactory('AnniversaryToken');
+  const anniversaryToken = await AnniversaryToken.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await anniversaryToken.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(
+    `AnniversaryToken with 1 ETH deployed to ${anniversaryToken.address}`
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
