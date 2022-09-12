85aa40d214734c45e7516e79c3d981a4a9f8bc58
shwld <shu.account@outlook.com>
2022-09-13 06:30:25 +0900

    👷chore: deploy to goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env
+        .ALCHEMY_API_KEY!}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
