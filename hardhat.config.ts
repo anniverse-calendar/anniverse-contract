c09f7d7c4599186fe31506fd6b93f9873dbc21cc
shwld <shu.account@outlook.com>
2022-09-19 14:47:11 +0900

    ✨feat: テスト環境なしでも立ち上げられる

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,11 +7,15 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
   networks: {
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env
-        .ALCHEMY_API_KEY!}`,
-      accounts: [process.env.GOERLI_PRIVATE_KEY!],
-    },
+    ...(process.env.ALCHEMY_API_KEY
+      ? {
+          goerli: {
+            url: `https://eth-goerli.alchemyapi.io/v2/${process.env
+              .ALCHEMY_API_KEY!}`,
+            accounts: [process.env.GOERLI_PRIVATE_KEY!],
+          },
+        }
+      : {}),
   },
 };
 
