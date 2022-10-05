dba7aeda6cc4b601d07281a22e458bf6bfa4f628
shwld <shu.account@outlook.com>
2022-10-06 08:52:31 +0900

    👷chore: update goerli token

--- config/contants.ts
+++ config/contants.ts
@@ -1,4 +1,4 @@
 export const CONTRACT_ADDRESS: { [name: string]: string } = {
-  goerli: '0xC8feE0e63eB141238f0E35Ae7D32C0E1BA6f0df5',
+  goerli: '0x47755671C516Af31064501DE8eEa72663729ACbf',
   localhost: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
 };
