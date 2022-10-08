0d760c565c3efc532538d8cfd9e6be4743704a5a
shwld <shu.account@outlook.com>
2022-10-08 12:44:03 +0900

    👷chore: deploy to goerli

--- config/contants.ts
+++ config/contants.ts
@@ -1,4 +1,4 @@
 export const CONTRACT_ADDRESS: { [name: string]: string } = {
-  goerli: '0xaD93C0D3bb7FE60dC29028E4E8A46b9c1dB0F2Be',
+  goerli: '0xd16Af4Da36028c740487C2c7fC852f9F57acC5B9',
   localhost: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
 };
